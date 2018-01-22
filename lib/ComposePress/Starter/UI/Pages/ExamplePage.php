56eeccbc7cb7b0acc41342be4f534bbd2bb43717
Derrick Hammer <derrick@derrickhammer.com>
2018-01-22 13:56:07 -0500

    *Fix up example page

--- lib/ComposePress/Starter/UI/Pages/ExamplePage.php
+++ lib/ComposePress/Starter/UI/Pages/ExamplePage.php
@@ -15,25 +15,25 @@ class ExamplePage extends \ComposePress\Settings\Abstracts\Page {
 	const NETWORK_CAPABILITY = 'manage_options';
 
 	public function register_settings() {
-		$tab      = Factory::tab( 'tab1', 'Tab 1', $this );
-		$tabb     = Factory::tab( 'tab1', 'Tab 2', $this );
-		$section  = Factory::section( 'section1', 'Section 1', $tab, 'blah' );
-		$sectionb = Factory::section( 'section1', 'Section 2', $tabb );
-		$sectionc = Factory::section( 'section3', 'Section 3', $this );
-		Factory::field( 'select_field', 'Select Field Multiple', Select::NAME, $section, [
+		$tab1     = Factory::tab( 'tab1', 'Tab 1', $this );
+		$tab2     = Factory::tab( 'tab2', 'Tab 2', $this );
+		$section  = Factory::section( 'section1', 'Section 1', $tab1, 'blah' );
+		$section1 = Factory::section( 'section2', 'Section 2', $tab2 );
+		$section3 = Factory::section( 'section3', 'Section 3', $this );
+		Factory::field( 'select_field_multiple', 'Select Field Multiple', Select::NAME, $section, [
 			'multiple' => true,
 			'options'  => [
 				'a' => 'b',
 				'c' => 'd',
 			],
 		] );
-		Factory::field( 'select_field', 'Select Field', Select::NAME, $section, [
+		Factory::field( 'select_field', 'Select Field', Select::NAME, $section1, [
 			'options' => [
 				'a' => 'b',
 				'c' => 'd',
 			],
 		] );
-		$testb_field = Factory::field( 'image_field', 'Image Field', Image::NAME, $sectionb );
-		$testc_field = Factory::field( 'wysiwyg_field', 'Wysiwyg Field', Wysiwyg::NAME, $sectionc );
+		$testb_field = Factory::field( 'image_field', 'Image Field', Image::NAME, $section1 );
+		$testc_field = Factory::field( 'wysiwyg_field', 'Wysiwyg Field', Wysiwyg::NAME, $section3 );
 	}
 }
