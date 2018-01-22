d9c88892135f5230b1785bcb2375295639887ac4
Derrick Hammer <derrick@derrickhammer.com>
2018-01-22 13:40:58 -0500

    *Add settings component *Add UI component *Add getter to example manager

--- lib/ComposePress/Starter/Plugin.php
+++ lib/ComposePress/Starter/Plugin.php
@@ -3,7 +3,9 @@
 namespace ComposePress\Starter;
 
 use ComposePress\Core\Abstracts\Plugin as PluginBase;
-use ComposePress\Starter\Example\Manager;
+use ComposePress\Settings;
+use ComposePress\Settings\Abstracts\UI;
+use ComposePress\Starter\Managers\Example;
 
 /**
  * Class Plugin
@@ -25,19 +27,31 @@ class Plugin extends PluginBase {
 	 */
 	private $example_component;
 	/**
-	 * @var Manager
+	 * @var Example
 	 */
 	private $example_manager;
+	/**
+	 * @var \ComposePress\Settings
+	 */
+	private $settings;
+	/**
+	 * @var \ComposePress\Settings\Abstracts\UI
+	 */
+	private $admin_ui;
 
 	/**
 	 * Plugin constructor.
 	 *
-	 * @param ExampleComponent $example_component
-	 * @param Manager          $example_manager
+	 * @param ExampleComponent                    $example_component
+	 * @param Example                             $example_manager
+	 * @param \ComposePress\Settings              $settings
+	 * @param \ComposePress\Settings\Abstracts\UI $admin_ui
 	 */
-	public function __construct( ExampleComponent $example_component, Manager $example_manager ) {
+	public function __construct( ExampleComponent $example_component, Example $example_manager, Settings $settings, UI $admin_ui ) {
 		$this->example_component = $example_component;
 		$this->example_manager   = $example_manager;
+		$this->settings          = $settings;
+		$this->admin_ui          = $admin_ui;
 		parent::__construct();
 	}
 
@@ -67,5 +81,26 @@ class Plugin extends PluginBase {
 	 */
 	public function get_example_component() {
 		return $this->example_component;
+	}
+
+	/**
+	 * @return \ComposePress\Settings
+	 */
+	public function get_settings() {
+		return $this->settings;
+	}
+
+	/**
+	 * @return \ComposePress\Settings\Abstracts\UI
+	 */
+	public function get_admin_ui() {
+		return $this->admin_ui;
+	}
+
+	/**
+	 * @return \ComposePress\Starter\Managers\Example
+	 */
+	public function get_example_manager() {
+		return $this->example_manager;
 	}
 }
