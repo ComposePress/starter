a24bb2de1d67155f6e1b84ecce90d7fcc9b7558c
Derrick Hammer <derrick@derrickhammer.com>
2018-01-22 13:42:16 -0500

    *Rename Pages to Page

--- lib/ComposePress/Starter/Managers/Page.php
+++ lib/ComposePress/Starter/Managers/Page.php
@@ -2,6 +2,6 @@
 
 namespace ComposePress\Starter\Managers;
 
-class Pages extends \ComposePress\Settings\Managers\Page {
+class Page extends \ComposePress\Settings\Managers\Page {
 	const MODULE_NAMESPACE = '\ComposePress\Starter\UI\Pages';
 }
