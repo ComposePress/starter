4142c94b27b064281c8dabe42e6d1610b0e298cd
Derrick Hammer <derrick@derrickhammer.com>
2018-01-22 13:50:07 -0500

    *Add example page to page manager

--- lib/ComposePress/Starter/Managers/Page.php
+++ lib/ComposePress/Starter/Managers/Page.php
@@ -4,4 +4,5 @@ namespace ComposePress\Starter\Managers;
 
 class Page extends \ComposePress\Settings\Managers\Page {
 	const MODULE_NAMESPACE = '\ComposePress\Starter\UI\Pages';
+	protected $modules = [ 'ExamplePage' ];
 }
